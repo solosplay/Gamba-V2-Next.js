cdd09028037b732f50e9c28aaeb9618c71c481eb
Bankk <106103625+BankkRoll@users.noreply.github.com>
2025-02-08 18:32:21 -0500

    fixes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,12 +12,11 @@ import {
   useTokenBalance,
 } from "gamba-react-ui-v2"
 import { PLATFORM_REFERRAL_FEE, TOKENLIST } from "@/constants"
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
-import { Modal } from "@/components/Modal"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Switch } from "@/components/ui/switch"
 import { toast } from "sonner"
@@ -34,9 +33,8 @@ export default function Header() {
   const balance = useTokenBalance()
   const { referrerAddress, isOnChain, referralStatus, referralLink, copyLinkToClipboard, clearCache } = useReferral()
 
-  const [showModal, setShowModal] = useState(false)
-  const [bonusHelp, setBonusHelp] = useState(false)
-  const [jackpotHelp, setJackpotHelp] = useState(false)
+  const [showBonusHelp, setShowBonusHelp] = useState(false)
+  const [showJackpotHelp, setShowJackpotHelp] = useState(false)
 
   const { isPriorityFeeEnabled, priorityFee, set } = useUserStore()
   const [newPriorityFee, setNewPriorityFee] = useState(priorityFee)
@@ -59,7 +57,6 @@ export default function Header() {
         context.setPool(token.mint)
       }
       toast.success(`Token set to ${token.name}`)
-      setShowModal(false)
     } catch (error) {
       toast.error("Error setting token")
     }
@@ -78,19 +75,9 @@ export default function Header() {
   const truncateString = (s: string, startLen = 4, endLen = startLen) =>
     s ? `${s.slice(0, startLen)}...${s.slice(-endLen)}` : ""
 
-  const handleConnect = () => {
-    if (wallet) {
-      connect().catch(() => {})
-    } else {
-      walletModal.setVisible(true)
-    }
-  }
-
-  useEffect(() => {
-    if (!connected && showModal) {
-      setShowModal(false)
-    }
-  }, [connected, showModal])
+  const handleConnect = useCallback(() => {
+    walletModal.setVisible(true)
+  }, [walletModal])
 
   return (
     <>
@@ -104,9 +91,11 @@ export default function Header() {
           </Link>
         </div>
         <div className="max-sm:text-xs max-sm:gap-1 flex gap-2.5 items-center relative">
-          {bonusHelp && (
-            <Modal onClose={() => setBonusHelp(false)}>
-              <h1>You have a bonus!</h1>
+          <Dialog open={showBonusHelp} onOpenChange={setShowBonusHelp}>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>You have a bonus!</DialogTitle>
+              </DialogHeader>
               <p>
                 You have{" "}
                 <b>
@@ -114,11 +103,14 @@ export default function Header() {
                 </b>{" "}
                 worth of free plays. This bonus will be applied automatically when you play.
               </p>
-            </Modal>
-          )}
-          {jackpotHelp && (
-            <Modal onClose={() => setJackpotHelp(false)}>
-              <div className="text-lg font-semibold text-center">{token.name} Jackpot Details</div>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog open={showJackpotHelp} onOpenChange={setShowJackpotHelp}>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>{token.name} Jackpot Details</DialogTitle>
+              </DialogHeader>
               {pool.jackpotBalance > 0 && (
                 <div className="flex text-[#003c00] rounded-lg bg-[#03ffa4] px-2.5 py-0.5 uppercase font-bold">
                   <TokenValue amount={pool.jackpotBalance} />
@@ -156,11 +148,12 @@ export default function Header() {
                   </Button>
                 </div>
               </div>
-            </Modal>
-          )}
+            </DialogContent>
+          </Dialog>
+
           {pool.jackpotBalance > 0 && (
             <button
-              onClick={() => setJackpotHelp(true)}
+              onClick={() => setShowJackpotHelp(true)}
               className="hidden md:flex all-unset cursor-pointer text-[#003c00] rounded-lg bg-[#03ffa4] px-2.5 py-0.5 text-xs uppercase font-bold transition-colors duration-200 hover:bg-white"
             >
               <TokenValue amount={pool.jackpotBalance} />
@@ -168,15 +161,15 @@ export default function Header() {
           )}
           {balance.bonusBalance > 0 && (
             <button
-              onClick={() => setBonusHelp(true)}
+              onClick={() => setShowBonusHelp(true)}
               className="hidden md:flex all-unset cursor-pointer text-[#003c00] rounded-lg bg-[#03ffa4] px-2.5 py-0.5 text-xs uppercase font-bold transition-colors duration-200 hover:bg-white"
             >
               +<TokenValue amount={balance.bonusBalance} />
             </button>
           )}
-          <Dialog open={showModal} onOpenChange={setShowModal}>
-            <DialogTrigger asChild>
-              {connected ? (
+          {connected ? (
+            <Dialog>
+              <DialogTrigger asChild>
                 <Button variant="outline">
                   <div className="flex items-center gap-2">
                     {token && (
@@ -193,134 +186,134 @@ export default function Header() {
                     <ChevronDown className="h-4 w-4 opacity-50" />
                   </div>
                 </Button>
-              ) : (
-                <Button onClick={handleConnect} variant="outline">
-                  {connecting ? "Connecting" : "Connect Wallet"}
-                </Button>
-              )}
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Wallet & Settings</DialogTitle>
-              </DialogHeader>
-              <div className="space-y-6">
-                <div className="space-y-4">
-                  <div className="flex items-center justify-between">
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Wallet & Settings</DialogTitle>
+                </DialogHeader>
+                <div className="space-y-6">
+                  <div className="space-y-4">
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <p className="text-sm font-medium">Connected Wallet</p>
+                        <p className="text-xs opacity-70">{truncateString(publicKey?.toString() || "", 8, 8)}</p>
+                      </div>
+                      <Avatar className="h-12 w-12">
+                        <AvatarImage src={wallet?.adapter.icon} alt="Wallet Icon" />
+                        <AvatarFallback>WL</AvatarFallback>
+                      </Avatar>
+                    </div>
                     <div>
-                      <p className="text-sm font-medium">Connected Wallet</p>
-                      <p className="text-xs opacity-70">{truncateString(publicKey?.toString() || "", 8, 8)}</p>
-                    </div>
-                    <Avatar className="h-12 w-12">
-                      <AvatarImage src={wallet?.adapter.icon} alt="Wallet Icon" />
-                      <AvatarFallback>WL</AvatarFallback>
-                    </Avatar>
-                  </div>
-                  <div>
-                    <p className="text-sm font-medium">Balance</p>
-                    <div className="flex items-center gap-2">
-                      <img className="w-8 h-8 rounded-full" src={token.image || "/placeholder.svg"} alt="Token" />
-                      <p className="text-2xl font-bold">
-                        <TokenValue amount={balance.balance} />
-                        {balance.bonusBalance > 0 && (
-                          <span className="text-sm ml-1">
-                            (+
-                            <TokenValue amount={balance.bonusBalance} /> Bonus)
-                          </span>
-                        )}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <ScrollArea className="h-[200px] rounded-md border p-4">
-                  <div className="space-y-2">
-                    {Object.values(TOKENLIST).map((token, index) => (
-                      <button
-                        key={index}
-                        className="flex items-center gap-2.5 px-3 py-2 rounded-lg w-full text-left hover:bg-accent"
-                        onClick={() => handleSetToken(token)}
-                      >
-                        <img
-                          className="w-8 h-8 rounded-full"
-                          src={token.image || "/placeholder.svg"}
-                          alt={token.symbol}
-                        />
-                        <div>
-                          <div>{token.symbol}</div>
-                          <div className="text-sm opacity-50">{token.name}</div>
-                        </div>
-                      </button>
-                    ))}
-                  </div>
-                </ScrollArea>
-                <div className="space-y-4">
-                  <p className="text-sm font-medium">Priority Fee</p>
-                  <div className="flex items-center justify-between">
-                    <span>Enable Priority Fee</span>
-                    <Switch
-                      checked={isPriorityFeeEnabled}
-                      onCheckedChange={(checked) => {
-                        set({ isPriorityFeeEnabled: checked })
-                        if (checked) {
-                          toast.success("Priority fee enabled")
-                        } else {
-                          toast.error("Priority fee disabled")
-                        }
-                      }}
-                    />
-                  </div>
-                  {isPriorityFeeEnabled && (
-                    <div className="space-y-2">
-                      <label className="text-sm">Priority Fee (Microlamports):</label>
-                      <div className="flex gap-2">
-                        <Input
-                          type="number"
-                          value={newPriorityFee}
-                          onChange={(e) => {
-                            const parsedValue = Number.parseInt(e.target.value, 10)
-                            if (!isNaN(parsedValue)) {
-                              setNewPriorityFee(parsedValue)
-                            }
-                          }}
-                        />
-                        <Button onClick={handleSetPriorityFee}>Set</Button>
+                      <p className="text-sm font-medium">Balance</p>
+                      <div className="flex items-center gap-2">
+                        <img className="w-8 h-8 rounded-full" src={token.image || "/placeholder.svg"} alt="Token" />
+                        <p className="text-2xl font-bold">
+                          <TokenValue amount={balance.balance} />
+                          {balance.bonusBalance > 0 && (
+                            <span className="text-sm ml-1">
+                              (+
+                              <TokenValue amount={balance.bonusBalance} /> Bonus)
+                            </span>
+                          )}
+                        </p>
                       </div>
                     </div>
-                  )}
-                </div>
-                <div className="space-y-4">
-                  <p className="text-sm font-medium">Referral Link</p>
-                  <div className="flex space-x-2">
-                    <Input value={`${location.origin}?code=${publicKey?.toString() || ""}`} readOnly />
-                    <Button onClick={copyInvite} variant="outline">
-                      <ClipboardCopy className="h-4 w-4 mr-2" />
-                      Copy
-                    </Button>
+                  </div>
+                  <ScrollArea className="h-[200px] rounded-md border p-4">
+                    <div className="space-y-2">
+                      {Object.values(TOKENLIST).map((token, index) => (
+                        <button
+                          key={index}
+                          className="flex items-center gap-2.5 px-3 py-2 rounded-lg w-full text-left hover:bg-accent"
+                          onClick={() => handleSetToken(token)}
+                        >
+                          <img
+                            className="w-8 h-8 rounded-full"
+                            src={token.image || "/placeholder.svg"}
+                            alt={token.symbol}
+                          />
+                          <div>
+                            <div>{token.symbol}</div>
+                            <div className="text-sm opacity-50">{token.name}</div>
+                          </div>
+                        </button>
+                      ))}
+                    </div>
+                  </ScrollArea>
+                  <div className="space-y-4">
+                    <p className="text-sm font-medium">Priority Fee</p>
+                    <div className="flex items-center justify-between">
+                      <span>Enable Priority Fee</span>
+                      <Switch
+                        checked={isPriorityFeeEnabled}
+                        onCheckedChange={(checked) => {
+                          set({ isPriorityFeeEnabled: checked })
+                          if (checked) {
+                            toast.success("Priority fee enabled")
+                          } else {
+                            toast.error("Priority fee disabled")
+                          }
+                        }}
+                      />
+                    </div>
+                    {isPriorityFeeEnabled && (
+                      <div className="space-y-2">
+                        <label className="text-sm">Priority Fee (Microlamports):</label>
+                        <div className="flex gap-2">
+                          <Input
+                            type="number"
+                            value={newPriorityFee}
+                            onChange={(e) => {
+                              const parsedValue = Number.parseInt(e.target.value, 10)
+                              if (!isNaN(parsedValue)) {
+                                setNewPriorityFee(parsedValue)
+                              }
+                            }}
+                          />
+                          <Button onClick={handleSetPriorityFee}>Set</Button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="space-y-4">
+                    <p className="text-sm font-medium">Referral Link</p>
+                    <div className="flex space-x-2">
+                      <Input value={`${window.location.origin}?code=${publicKey?.toString() || ""}`} readOnly />
+                      <Button onClick={copyInvite} variant="outline">
+                        <ClipboardCopy className="h-4 w-4 mr-2" />
+                        Copy
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="flex justify-between">
+                    <DialogClose asChild>
+                      <Link href="/profile" passHref legacyBehavior>
+                        <Button variant="outline" className="flex items-center">
+                          <User className="h-4 w-4 mr-2" />
+                          View Profile
+                        </Button>
+                      </Link>
+                    </DialogClose>
+                    <DialogClose asChild>
+                      <Button
+                        variant="destructive"
+                        onClick={() => {
+                          disconnect()
+                        }}
+                      >
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Disconnect
+                      </Button>
+                    </DialogClose>
                   </div>
                 </div>
-                <div className="flex justify-between">
-                  <DialogClose asChild>
-                    <Link href="/profile" passHref legacyBehavior>
-                      <Button variant="outline" className="flex items-center">
-                        <User className="h-4 w-4 mr-2" />
-                        View Profile
-                      </Button>
-                    </Link>
-                  </DialogClose>
-                  <DialogClose asChild>
-                    <Button
-                      variant="destructive"
-                      onClick={() => {
-                        disconnect()
-                      }}
-                    >
-                      <LogOut className="h-4 w-4 mr-2" />
-                      Disconnect
-                    </Button>
-                  </DialogClose>
-                </div>
-              </div>
-            </DialogContent>
-          </Dialog>
+              </DialogContent>
+            </Dialog>
+          ) : (
+            <Button onClick={handleConnect} variant="outline">
+              {connecting ? "Connecting" : "Connect Wallet"}
+            </Button>
+          )}
         </div>
       </div>
     </>
